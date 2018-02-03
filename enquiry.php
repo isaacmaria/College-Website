53d4d76ee7abc6a6ea70615daca383f6b9e8b888
Isaac Maria <isaacmaria2@yahoo.com>
2018-02-03 14:58:24 +0530

    Add files via upload

---
+++ enquiry.php
@@ -0,0 +1,264 @@
+<!DOCTYPE HTML>
+<HEAD>
+<script>
+
+function validForm() {
+
+var x = document.forms["enquiry"]["fname"].value;
+var y = document.forms["enquiry"]["eemail"].value;
+
+if (x==null || x==""||y==null||y=="") {
+alert("Details must be filled out");
+return false;
+}
+else
+{
+window.confirm("Are you sure you want to submit?");
+
+}
+
+
+
+}
+
+function validEmail()
+{
+
+var y = document.forms["enquiry"]["eemail"].value;
+var atpos = y.indexOf("@");
+var dotpos = y.lastIndexOf(".");
+
+if(atpos<1||dotpos<(atpos+2)||(dotpos+2)>=y.length){
+alert("Not a valid E-mail address");
+return false;
+}
+
+}
+
+
+</script>
+
+
+
+
+<style>
+header{
+  border:1px solid grey;
+    margin:1px;margin-bottom:5px;padding:8px;
+    background-color:black;
+
+}
+
+section{
+
+  border:2px solid grey;
+    margin:1px;margin-bottom:10px;padding:8px;
+    background-color:	#b1b1b1;
+
+
+}
+
+article{
+
+  border:2px solid grey;
+    margin:10px;padding:18px;
+    background-color:	#e1e1e1;
+
+
+}
+
+
+p#title{
+font-size:300%;
+text-align:center;
+color:white;
+font-family:verdana;
+
+}
+nav{
+margin-top:5px;
+}
+
+
+
+body{
+background-color:lightgrey;
+
+}
+ 
+figure#tb{
+margin:0px 5px;
+
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 1px solid #e7e7e7;
+    background-color: #f3f3f3;
+    
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: #666;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #ddd;
+}
+
+
+li a.active {
+    color: white;
+    background-color: grey;
+
+}
+
+
+</style>
+</HEAD>
+<BODY>
+
+
+
+<header>
+<p id="title"> FALTU COLLEGE OF ENGINEERING </p>
+</header>
+<figure id = "tb">
+  <img src="a.jpg" alt="FALTU COLLEGE OF ENGINEERING" width="100%" height="200px">
+  <figcaption>Faltu College of Engineering (Autonomous College Affiliated to University of Mumbai)</figcaption>
+</figure>
+<nav>
+<ul>
+<li><a href="homepage.html">ABOUT US</a></li>
+<li><a href="academics.html">ACADEMICS</a></li>
+<li><a class="active" href="placement.html">PLACEMENT</a></li>
+<li><a href="admission.html">ADMISSION</a></li>
+<li><a href="contactus.html">CONTACT US</a></li>
+<ul>
+</nav>
+<article>
+<h1 style="font-size:220%;"> Placement Details </h1>
+<p>
+Faltu College of Engineering has Corporate Relations & Placement Cell dedicated for strengthening the interaction between Institute and Industry. The cell is coordinated by a senior faculty, who will be supported by one faculty member from each of the departments. The students also actively participate in the activities as volunteers.
+The basic purpose of this cell is to involve industry’s inputs in the form of expert lectures, short-term training for faculty members/ students, projects, laboratory development, sponsoring technical non technical events, placements, etc.
+</p>
+
+<h2> Facilities made available to the students</h2>
+
+<p>1. Arranging Campus Interviews of various organizations for final year students for their recruitments</p>
+<p>2. Arranging guest lectures of Eminent Personalities from Industry for career counseling</p>
+<p>3. Co-ordination for Industrial visits arranged by various departments.</p>
+<p>4. Co-ordinations with the various departments through T & P for arranging Technical Seminars, Arranging Mock Interviews, Written Test & Group Discussion for the final year students.</p>
+</article>
+<article>
+<h1 style="font-size:220%;"> Placement Policy</h2>
+
+<h3>Purpose</h3>
+<p>This policy lays down the procedures for assisting the students to find placement in the industry at the end of the program. The policy also highlights the Standard operating procedures  to be followed by Final Year students of UG and PG Courses.</p>
+<h3>Scope</h3>
+<p>Procedures laid down in this document are applicable to all the eligible and interested students of final year (UG & PG courses).</p>
+</article>
+
+<article>
+<h1 style="font-size:220%;">Placement Procedure and Guidelines</h1>
+<p>
+
+
+1.    Invitation to be sent to the companies for Campus Recruitment Program. <br>
+2.    Invitation to be sent to distinguished alumni of Institute working at senior positions for Campus Recruitment Program.<br>
+3.    If an organization is offered a Dream Status by college (where the Company CTC is Rs.5.00 LPA and above ) all eligible students are allowed to register.<br>
+4.    Once registered, student will be required to attend the pre-placement talk.<br>
+5.    Once the student opts to appear for the Aptitude Test, the entire cycle has to be completed, i.e. if short listed for Group Discussion and/ or Personal Interview; the candidate MUST attend.<br>
+6.    After Registration for Recruitment Process, if any student is absent for Recruitment process, he must submit an application to Training & Placement Officer with genuine reason.<br>
+7.    Students should not directly interact with HR Executives of Company before Pre-placement Talk.<br>
+8.    Students should keep, Xerox copies of Mark sheets and certificates during the recruitment process     <br>  
+9.    It is advised to keep ready 2 Passport size photographs and a hard copy of resume. <br>
+10.    Students should keep Xerox copies of Pan Card and Aadhar Card or Passport during the recruitment process. <br>
+11.    Wearing formal attire during the recruitment process is Mandatory <br>
+12.    No Candidate/Student will be entertained in Campus Recruitment Program (CRP) after the deadline of registration. <br>
+13.    Students with Poor attendance/academic performance and cases of indiscipline/malpractices will not be eligible for participation in the placement process. <br>
+14.    Student coordinators should be mix of 4(1 TE + 2 BE + 1ME) students. <br>
+15.    Information about the schedule of recruitment process will be communicated to the students on e-mail ID, Notice board, Facebook whichever is feasible. <br>
+16.    Announcements regarding recruitment process will be posted to Facebook, email ID and Notice Board from time to time. <br>
+17.    Interested students are required to register via the link displayed on Facebook and email ID. <br>
+18.    Usually a Pre Placement Talk (PPT) is conducted by the organization explaining the students; job profile, service agreement etc. The students are requested to get any doubts clarified during this talk.(including the doubts regarding CTC,Fixed pay, variable pay and other details regarding CTC) <br>
+19.    A database of the students will be created as per company database format. <br>
+20.    Students should mention in CV/Bio-data Interested area of working. <br>
+21.    Mail the LATEST copy of your resume whenever asked for. <br>
+22.    During the Recruitment process, student should submit CV/Bio-data in the format given by Institute with template of KJ Somaiya College of Engineering.(Flexibility will be allowed as per requirement to edit CV) <br>
+23.    Students are required to remain present at least 15 minutes before the scheduled time of PPT or aptitude test. Late comers will not be allowed to appear for the process.  <br>
+24.    Students are allowed to take maximum two job offers (including dream).<br>
+25.    If selected in a dream company, students are not allowed to appear for placement for any other dream company. <br>
+26.    If a student is selected in two companies, he/she will be asked to join the second company but with option of whichever company gives offer letter first. <br>
+27.    Selected student must submit Xerox copy of offer letter to Training & Placement Officer and the Department Faculty coordinator<br>
+</p>
+</article>
+
+<h1> ENQUIRY </h1>
+<section>
+<article>
+
+<fieldset>
+<legend>    ENQUIRY </legend>
+
+<form name="enquiry" onsubmit="return validForm();return validEmail()" method="post">
+Full Name : 
+<input type="text" name="fname"><br>
+Email : 
+<input type="email" name="eemail"><br>
+Comments : <br>
+<textarea name="ecomment" rows="10" cols="40"></textarea><br>
+
+<br><br>
+
+<input type="submit" name="submit">
+</form>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "college";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+  if (isset($_POST['submit']))
+  {
+$fullname =  $_POST['fname'];
+$email = $_POST['eemail'];
+$comment = $_POST['ecomment'];
+}
+
+$sql = "INSERT INTO enquiry(Name, Email, Queries)
+VALUES ('$fullname', '$email', '$comment')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
+</fieldset>
+
+
+</article>
+</section>
+</BODY>
+</HTML>
---
+++ staff.php
@@ -0,0 +1,159 @@
+
+<!DOCTYPE HTML>
+<HEAD>
+
+
+<style>
+header{
+  border:1px solid grey;
+    margin:1px;margin-bottom:5px;padding:8px;
+    background-color:black;
+
+}
+
+section{
+
+  border:2px solid black;
+    margin:1px;margin-bottom:1px;padding:8px;
+    background-color:	#717171;
+
+
+}
+
+p#title{
+font-size:300%;
+text-align:center;
+color:white;
+font-family:verdana;
+
+}
+nav{
+margin-top:5px;
+}
+
+
+
+body{
+background-color:lightgrey;
+
+}
+ 
+figure#tb{
+margin:0px 5px;
+
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 1px solid #e7e7e7;
+    background-color: #f3f3f3;
+   
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: #666;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #ddd;
+	
+}
+
+
+.active {
+    color: white;
+    background-color: grey;
+
+}
+
+article{
+
+  border:2px solid grey;
+    margin:10px;padding:18px;
+    background-color:	#e1e1e1;
+
+
+}
+
+
+
+</style>
+</HEAD>
+<BODY>
+<header>
+<p id="title"> FALTU COLLEGE OF ENGINEERING </p>
+</header>
+<figure id = "tb">
+  <img src="a.jpg" alt="FALTU COLLEGE OF ENGINEERING" width="100%" height="200px">
+  <figcaption>Faltu College of Engineering (Autonomous College Affiliated to University of Mumbai)</figcaption>
+</figure>
+<nav>
+<ul>
+<li><a class="active" href="homepage.html">ABOUT US</a></li>
+<li><a href="academics.html">ACADEMICS</a></li>
+<li><a href="placement.html" >PLACEMENT</a></li>
+<li><a href="placement.html" >ADMISSION</a></li>
+<li><a href="contactus.html">CONTACT US</a></li>
+<ul>
+</nav>
+<section>
+
+
+<article>
+<a href="fe_applicants.php" target="_blank"><h2>Click here to know the details about FE applicants</h2></a>
+
+
+
+
+</article>
+
+
+
+<article>
+<a href="se_applicants.php"><h2>Click here to know the details about SE applicants</h2></a>
+
+
+
+
+</article>
+
+<article>
+<a href="staff.php"><h2>Click here to know the details about Staff</h2></a>
+<?php
+$con=mysqli_connect("localhost","root","","college");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM Staff");
+
+while($row = mysqli_fetch_array($result)) {
+  echo $row['Name'] . " " . $row['Faculty'] . " " .$row['Salary'] . " " .$row['Age'];
+  echo "<br>";
+}
+
+mysqli_close($con);
+?>
+</article>
+
+<article>
+<a href="toppers.php"><h2>Click here to know the details about Toppers</h2></a>
+
+</article>
+
+</section>
+
+</BODY>
+</HTML>
